5a5b760450ae86c6b62adfcccae25d095ebfa4d5
masumbillah6778 <102429423+masumbillah6778@users.noreply.github.com>
2024-11-18 14:55:45 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,141 @@
+import vPlayer from "./vPlayer.js";
+
+const videoList = [
+  {
+    url: "videos/(1).mp4",
+    title: "Tania",
+    desc: "Tania Tiktok Video",
+    img: "#",
+  },
+
+  {
+    url: "videos/(2).mp4",
+    title: "Tania",
+    desc: "Tania Tiktok Video",
+    img: "#",
+  },
+
+  {
+    url: "videos/(3).mp4",
+    title: "Tania",
+    desc: "Tania Tiktok Video",
+    img: "#",
+  },
+
+  {
+    url: "videos/(4).mp4",
+    title: "Tania",
+    desc: "Tania Tiktok Video",
+    img: "#",
+  },
+
+  {
+    url: "videos/(5).mp4",
+    title: "Tania",
+    desc: "Tania Tiktok Video",
+    img: "#",
+  },
+
+  {
+    url: "videos/(6).mp4",
+    title: "Tania",
+    desc: "Tania Tiktok Video",
+    img: "#",
+  },
+
+  {
+    url: "videos/(7).mp4",
+    title: "Tania",
+    desc: "Tania Tiktok Video",
+    img: "#",
+  },
+
+  {
+    url: "videos/(8).mp4",
+    title: "Tania",
+    desc: "Tania Tiktok Video",
+    img: "#",
+  },
+
+  {
+    url: "videos/(9).mp4",
+    title: "Tania",
+    desc: "Tania Tiktok Video",
+    img: "#",
+  },
+
+  {
+    url: "videos/(10).mp4",
+    title: "Tania",
+    desc: "Tania Tiktok Video",
+    img: "#",
+  },
+];
+
+// Videos Wrapper
+const videosWrapper = document.querySelector(".videos");
+
+// loader
+const loader = document.querySelector(".loader");
+
+setTimeout(() => {
+  injectVideos();
+  loader.style.display = "none";
+}, 2000);
+
+function injectVideos() {
+  videoList.forEach((video) => {
+    videosWrapper.innerHTML += vPlayer(video);
+  });
+
+  const vids = document.querySelectorAll(".vdo-wrapper");
+
+  vids.forEach((vid) => {
+    // video
+    const vdo = vid.querySelector(".vdo");
+
+    // handle pause play
+    const pausePlayBtn = vid.querySelector("#pause-play-btn");
+    pausePlayBtn.addEventListener("click", () => {
+      if (vdo.paused) {
+        vdo.play();
+        pausePlayBtn.innerHTML = '<i class="fa-solid fa-play"></i>';
+      } else {
+        vdo.pause();
+        pausePlayBtn.innerHTML = '<i class="fa-solid fa-pause"></i>';
+      }
+    });
+
+    // handle like
+    const likeBtn = vid.querySelector("#like-btn");
+    likeBtn.addEventListener("click", () => {
+      likeBtn.classList.toggle("color-switch");
+      dislikeBtn.classList.remove("color-switch");
+    });
+
+    // handle dislike
+    const dislikeBtn = vid.querySelector("#dislike-btn");
+    dislikeBtn.addEventListener("click", () => {
+      dislikeBtn.classList.toggle("color-switch");
+      likeBtn.classList.remove("color-switch");
+    });
+
+    // handle dislike
+    const commentsBtn = vid.querySelector("#comments-btn");
+    const comments = vid.querySelector(".comments");
+    commentsBtn.addEventListener("click", () => {
+      comments.classList.toggle("toggle-comments");
+    });
+
+    // handle share
+    const shareBtn = vid.querySelector("#share-btn");
+    const vidTitle = vid.querySelector(".info-wrapper h3");
+    shareBtn.addEventListener("click", () => {
+      navigator.share({
+        title: vidTitle.textContent,
+        text: "text",
+        url: vdo.src,
+      });
+    });
+  });
+}
